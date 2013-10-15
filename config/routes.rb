f8d09b1de455d1de13d372a459ffdc9654e5ddfd
Luis Bajana <lbajana@gmail.com>
2013-10-15 12:19:53 -0500

    Scaffolded Article Model, and integration with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bootstraprails::Application.routes.draw do
+  resources :articles
+
+  root :to => "articles#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
